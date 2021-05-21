1d9bf195acce14e825943a4c0466d30653a718a1
TeraMike <57662035+TeraMike@users.noreply.github.com>
2021-05-21 16:00:28 +0100

    Fixed print + optimisation
    How did that sneak through?? Tidied it up and included the ceil(sqrt()) functions for optimising.

--- Model Code/python/specific_exercises/primes.py
+++ Model Code/python/specific_exercises/primes.py
@@ -3,23 +3,32 @@
 # So, primes can be found by checking if every number from 2 to number-1 is a factor of number.
 # You can save time by just checking up to the square root of the prime.
 #    --> A composite number must have a factor less than its square root.
+#    --> (It can't have two factors larger than its square root)
 
-import math  # We'll use this for the square root
+import math  # We'll use this for square root and rounding.
 
 print("Let's find a prime number! WARNING: large numbers may take a long time to check.")
-number = int(input("Enter the number to see if it's prime: "))
+number = int(input("Enter a number to see if it's prime: "))
 
-if number > 1:  # 1 is not a prime
+if number > 1:  # 1 is not a prime number! It has only one factor, itself.
+    rangeLimit = math.ceil(math.sqrt(number))  # Math.ceil rounds up. range() likes integers.
 
-    for i in range(2, number - 1):
+    for i in range(2, rangeLimit):
         if number % i == 0:  # 'modulo' (%) checks how many times a number divides into another number
                           # if modulo is zero, that number is a factor of the other. e.g.: 4 % 2 = 0
-            print(number + " is not a prime number.")
-            print(i + "is a factor of " + number + ". " + i + "times" + number//i + "is" + number)
+            print("\n" + str(number) + " is not a prime number.")
+
+            numStr = str(number)
+            iterStr = str(i)
+            pairedFactor = str(number//i)
+
+            print(iterStr + " is a factor of " + numStr + ".")
+            print(iterStr + " x " + pairedFactor + " = " + numStr)
+            # An extension would be to get all factors of the number!
             break
     else:  # Make sure this else is indented to the same level as the 'for i in range' line!
            # What happens if it's indented to match the if statement following that line?
-        print(number + "is a prime number!")
+        print(str(number) + " is a prime number!")
 
 else:
     print("1 is not a prime number.")
